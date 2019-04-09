1a9bcf7c1d09b2dfaa70d5e28a1eaffa49399666
srsimps19 <srsimps19@gmail.com>
2019-04-09 14:31:58 -0500

    added query selector for nav items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+function getNavItems(){
+    const header = document.querySelector('.main-navigation');
+    const navLinks = header.querySelectorAll('nav-link');
+    return {
+        header, 
+        logoHeading: header.querySelector('.logo-heading'),
+        nav: header.querySelector('nav'),
+        home: navLinks[0],
+        aboutUs: navLinks[1],        
+        blog: navLinks[2],
+        contact: navLink[3],
+    }
+} 
+
