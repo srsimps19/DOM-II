5f44382c9778a7999255b35d316d9692f9eb3b16
srsimps19 <srsimps19@gmail.com>
2019-04-09 17:07:02 -0500

    added scroll animation for all images

--- js/index.js
+++ js/index.js
@@ -3,15 +3,21 @@
 //                   Main
 // ===============================================
 
-window.onload = function() {
-    alert('Welcome to the fun bus!')
-}
-wireNavItems();
-wireH2s();
+window.onload = greetUser
+prepareImagesForAnimation()
+wireImgScrollAnimation()
+wireNavItems()
+wireH2s()
+
 
 // ===============================================
 //                  Helpers
 // ===============================================
+
+function greetUser() {
+    alert('Welcome to the Fun Bus!')
+    console.log('welcome')
+}
 
 function wireNavItems() {
     const navItems = [...document.querySelectorAll('nav .nav-link')]
@@ -38,10 +44,55 @@ function wireH2s() {
     })
 }
 
-function greetUser() {
-    alert('Welcome to the Fun Bus!')
-    console.log('welcome')
+function prepareImagesForAnimation() {
+    const funBus = document.getElementById('funBus') // l
+    const adventure = document.getElementById('adventure') // r
+    const fun = document.getElementById('fun') // l
+    const destination = document.getElementById('destination') // r
+
+    funBus.classList.add('left', 'slide-in')
+    fun.classList.add('left', 'slide-in')
+
+    adventure.classList.add('right', 'slide-in')
+    destination.classList.add('right', 'slide-in')
 }
+
+function wireImgScrollAnimation () {
+    window.addEventListener('scroll', debounce(checkSlide));
+}
+
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  
+  function checkSlide() {
+    const sliderImages = document.querySelectorAll('img');
+    sliderImages.forEach(sliderImage => {
+      // half way through the image
+      const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+      // bottom of the image
+      const imageBottom = sliderImage.offsetTop + sliderImage.height;
+      const isHalfShown = slideInAt > sliderImage.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+      if (isHalfShown && isNotScrolledPast) {
+        sliderImage.classList.add('active');
+      } else {
+        sliderImage.classList.remove('active');
+      }
+    });
+  }
 
 // function renderModal(event) {
 //     event.target.classList.remove('modal--on')
